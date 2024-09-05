ea0168dac7e2b2e3684fe442ba87d2973920f90e
PedroHenrique1606 <pedromelo.dev.contato@gmail.com>
2024-09-05 13:46:35 -0300

    initialize project and setup

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native'
+
+export default function Index() {
+    return (
+        <View className='bg-red-500'>
+            <Text>Hello, mofi</Text>
+        </View>
+    )
+}
